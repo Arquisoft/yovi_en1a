519c09e1e2ca13be3986cbf33f7fa6f1036298c3
emresen12 <emresen696@icloud.com>
2026-03-08 01:31:53 +0100

    added gameboard ui test2

--- webapp/src/__tests__/GameBoard.test.tsx
+++ webapp/src/__tests__/GameBoard.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
 import { describe, it, expect } from 'vitest';
 import GameBoard from '../GameBoard.tsx';
 
